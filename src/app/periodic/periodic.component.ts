594ab849fcd51941729e1f4ce5c3d35fdcae0ff7
kudane <kraisorn204@gmail.com>
2021-05-25 21:59:27 +0700

    2 files modified

--- src/app/periodic/periodic.component.ts
+++ src/app/periodic/periodic.component.ts
@@ -1,7 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { distinctUntilChanged, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-periodic',
   templateUrl: './periodic.component.html'
 })
-export class PeriodicComponent {}
+export class PeriodicComponent implements OnInit, AfterViewInit, OnDestroy {
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+
+    this.route.queryParams.pipe(
+      distinctUntilChanged(),
+      tap(params => console.log('params', params))
+    );
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
+}
