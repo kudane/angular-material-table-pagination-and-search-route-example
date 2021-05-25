33b51445e5d429b9c2a789886adc5917efe3d7a9
kudane <kraisorn204@gmail.com>
2021-05-25 21:33:08 +0700

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnDestroy, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MaterialModule } from './material.module';
-import { SharedModule } from './shared.module';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    SharedModule,
-    MaterialModule
-  ],
+  imports: [BrowserModule, BrowserAnimationsModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
